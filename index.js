1446dc87249beb6b52dc59734f1f2b23ffa9c916
msm <msiemieniukmorawski@gmail.com>
2020-05-18 20:43:22 +0200

    Add  more interestingUrls, add firstName, email and pets data

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const typeDefs = gql`
   type Query {
     greeting: String
     interestingUrls: [String]
+    firstName: String
+    email: String
+    pets: [String]
   }
 `;
 const data = {
   greeting: 'Hellow worl!',
-  interestingUrls: ['https://kursreacta.pl', 'http://ms-m.pl'],
+  interestingUrls: [
+    'https://kursreacta.pl/',
+    'http://ms-m.pl/',
+    'https://scrimba.com/',
+    'https://edu.devstyle.pl/',
+  ],
+  firstName: 'John',
+  email: 'john@example.com',
+  pets: ['Mittens', 'Doggo', 'Birb'],
 };
 const server = new ApolloServer({
   typeDefs,
