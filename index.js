8050cbbc0d5993afb90c3eb874d3a0ebe4c08f3b
msm <msiemieniukmorawski@gmail.com>
2020-05-18 20:39:12 +0200

    Add greeting and interestingUrls

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server');
 
-const typeDefs = `
-    type Query{
-        greeting: String
-    }
+const typeDefs = gql`
+  type Query {
+    greeting: String
+    interestingUrls: [String]
+  }
 `;
+const data = {
+  greeting: 'Hellow worl!',
+  interestingUrls: ['https://kursreacta.pl', 'http://ms-m.pl'],
+};
 const server = new ApolloServer({
   typeDefs,
+  rootValue: data,
 });
 
 server.listen({ port: 4000 }).then((result) => console.log(result.url));
